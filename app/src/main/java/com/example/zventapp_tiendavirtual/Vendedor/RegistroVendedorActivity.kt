3165c14a0661bd9b835b3e5963c0ef284ce8fc69
AbrahamPadrino <abrahampadrino@gmail.com>
2025-03-05 11:09:00 -0500

    Registro de Vendedor UI

---
+++ app/src/main/java/com/example/zventapp_tiendavirtual/Vendedor/RegistroVendedorActivity.kt
@@ -0,0 +1,13 @@
+package com.example.zventapp_tiendavirtual.Vendedor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.zventapp_tiendavirtual.R
+
+class RegistroVendedorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_vendedor)
+
+    }
+}
