c8732255c62ba15afe4e32698d8a33314bb4767e
AbrahamPadrino <abrahampadrino@gmail.com>
2025-04-04 10:46:51 -0500

    Implementado: Cargar Categorias desde la BD

---
+++ app/src/main/java/com/example/zventapp_tiendavirtual/Modelos/ModeloCategoria.kt
@@ -0,0 +1,14 @@
+package com.example.zventapp_tiendavirtual.Modelos
+
+class ModeloCategoria {
+
+    var id : String = ""
+    var categoria : String = ""
+
+    constructor()
+
+    constructor(id: String, categoria: String) {
+        this.id = id
+        this.categoria = categoria
+    }
+}
--- app/src/main/java/com/example/zventapp_tiendavirtual/Vendedor/Productos/AgregarProductoActivity.kt
+++ app/src/main/java/com/example/zventapp_tiendavirtual/Vendedor/Productos/AgregarProductoActivity.kt
@@ -5,12 +5,18 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.zventapp_tiendavirtual.Adaptadores.AdaptadorImagenSeleccionada
 import com.example.zventapp_tiendavirtual.Constantes
+import com.example.zventapp_tiendavirtual.Modelos.ModeloCategoria
 import com.example.zventapp_tiendavirtual.Modelos.ModeloImagenSeleccionada
 import com.example.zventapp_tiendavirtual.databinding.ActivityAgregarProductoBinding
 import com.github.dhaval2404.imagepicker.ImagePicker
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class AgregarProductoActivity : AppCompatActivity() {
 
@@ -20,10 +26,15 @@ class AgregarProductoActivity : AppCompatActivity() {
     private lateinit var imagenSelecArrayList : ArrayList<ModeloImagenSeleccionada>
     private lateinit var adaptadorImagenSel : AdaptadorImagenSeleccionada
 
+    private lateinit var categoriasArrayList : ArrayList<ModeloCategoria>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgregarProductoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Cargar Categorias en el TextView
+        cargarCategorias()
 
         imagenSelecArrayList = ArrayList()
 
@@ -31,8 +42,52 @@ class AgregarProductoActivity : AppCompatActivity() {
             seleccionarImg()
         }
 
+        binding.etCategoria.setOnClickListener {
+            selectCategoria()
+        }
+
         cargarImagenes()
 
+    }
+
+    private fun cargarCategorias() {
+        categoriasArrayList = ArrayList()
+        val ref = FirebaseDatabase.getInstance().getReference("Categorias").orderByChild("categoria")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+               categoriasArrayList.clear()
+                // Leer en Tiempo Real los registros de la Base de Datos en la Tabla "Categorias"
+                for (ds in snapshot.children){
+                    val modelo = ds.getValue(ModeloCategoria::class.java)
+                    categoriasArrayList.add(modelo!!)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+   // Visualizar las Categorias  a través de un AlertDialog
+    private var idCat = ""
+    private var tituloCat = ""
+    private fun selectCategoria() {
+        val categoriasArray = arrayOfNulls<String>(categoriasArrayList.size)
+
+        for (i in categoriasArray.indices){
+            categoriasArray[i] = categoriasArrayList[i].categoria
+        }
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Seleccione la Categoría")
+            .setItems(categoriasArray){dialog, witch ->
+
+                idCat = categoriasArrayList[witch].id
+                tituloCat = categoriasArrayList[witch].categoria
+                binding.etCategoria.text = tituloCat
+            }
+            .show()
     }
 
     private fun cargarImagenes() {
